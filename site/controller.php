d379b85576cbb0b1fceceb3494264146fbc12ed5
Norbert Kuemin <momo_102@bluemail.ch>
2017-07-20 08:20:57 +0100

    Fix installation

---
+++ site/controller.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* @version	$Id$
+* @package	Joomla
+* @subpackage	NoKWebDAV
+* @copyright	Copyright (c) 2017 Norbert Kümin. All rights reserved.
+* @license	http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE
+* @author	Norbert Kuemin
+* @authorEmail	momo_102@bluemail.ch
+*/
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla controller library
+jimport('joomla.application.component.controller');
+ 
+/**
+ * Hello World Component Controller
+ */
+class NoKWebDAVController extends JControllerLegacy {
+	/**
+	 * Method to display a view.
+	 *
+	 * @param   boolean			If true, the view output will be cached
+	 * @param   array  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
+	 *
+	 * @return  JController		This object to support chaining.
+	 * @since   1.5
+	 */
+	public function display($cachable = false, $urlparams = false) {
+		$cachable = true;
+		// Set the default view name and format from the Request.
+		$vName = $this->input->get('view', 'access');
+		$this->input->set('view', $vName);
+		$safeurlparams = array('catid' => 'INT', 'id' => 'INT', 'cid' => 'ARRAY', 'year' => 'INT', 'month' => 'INT', 'limit' => 'UINT', 'limitstart' => 'UINT',
+			'showall' => 'INT', 'return' => 'BASE64', 'filter' => 'STRING', 'filter_order' => 'CMD', 'filter_order_Dir' => 'CMD', 'filter-search' => 'STRING', 'print' => 'BOOLEAN', 'lang' => 'CMD');
+		parent::display($cachable, $safeurlparams);
+		return $this;
+	}
+}
+?>
