e653623c39295562a06e721e09fd098a6df9664c
Norbert Kuemin <momo_102@bluemail.ch>
2017-07-24 12:10:44 +0100

    Logging is working

--- admin/helpers/webdav.php
+++ admin/helpers/webdav.php
@@ -11,7 +11,7 @@
 
 // No direct access
 defined('_JEXEC') or die('Restricted access');
- 
+
 class WebDAVHelper {
 	public static $HTTP_STATUS_OK = '200';
 	public static $HTTP_STATUS_OK_PARTIAL = '206';
@@ -25,9 +25,6 @@ class WebDAVHelper {
 	public static $HTTP_STATUS_ERROR_LOCKED = '423';
 	public static $HTTP_STATUS_ERROR_NOT_IMPLEMENTED = '501';
 	private static $_allow = array();
-	private static $_debug = true;
-	private static $_debugFile = '/tmp/nokwebdav-{timestamp}.log';
-	private $_debugText = '';
 	private static $_http_status_text = array('200' => 'OK',
 		'206' => 'Partial Content',
 		'207' => 'Multi-Status',
@@ -72,28 +69,22 @@ class WebDAVHelper {
 			echo $content;
 		}
 */
-		$this->_debugSave();
 	}
 
 	public function debugAddMessage($message) {
-		if (self::$_debug === false) { return; }
-		$this->_debugText .= date('Y-m-d H:i:s');
-		$this->_debugText .= "\t$message\n";
+		JLog::add($message, JLog::DEBUG);
 	}
 
 	public function debugAddArray($list, $name) {
-		if (self::$_debug === false) { return; }
-		$message = $name." = {\n";
+		$entries = array();
 		foreach($list as $key => $value) {
-			$message .= "\t$key => $value\n";
+			$entries[] = "$key => $value";
 		}
-		$message .= '}';
-		$this->debugAddMessage($message);
+		$this->debugAddMessage($name.' = {'.join(', ',$entries).'}');
 	}
 
 	public function debugServerEnv() {
 		global $_SERVER;
-		if (self::$_debug === false) { return; }
 		$this->debugAddArray($_SERVER,'_SERVER');
 	}
 
@@ -109,19 +100,6 @@ class WebDAVHelper {
 		foreach($headers as $header) {
 			header($header,true);
 		}
-	}
-
-	private function _debugSave() {
-		global $_SERVER;
-		if (self::$_debug === false) { return; }
-		$filename = str_replace('{timestamp}',date('Ymd').'T'.date('His'),self::$_debugFile);
-foreach($_SERVER as $key => $value) {
-	header('x-comment-'.$key.': '.$value,true);
-}
-		$result = file_put_contents($filename, $this->_debugText);
-	}
-
-	private function _arrayToText($list, $name) {
 	}
 }
 ?>
--- site/nokwebdav.php
+++ site/nokwebdav.php
@@ -11,17 +11,30 @@
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
- 
+
 // import joomla controller library
 jimport('joomla.application.component.controller');
- 
+
+// Include the JLog class.
+jimport('joomla.log.log');
+
+// Initialise a basic logger with no options (once only).
+JLog::addLogger(
+	array(
+		'text_file' => 'nokwebdav.log',
+		'text_file_path' => '/tmp/',
+		'text_entry_format' => '{DATETIME} {PRIORITY} {MESSAGE}'
+	),
+	JLog::ALL
+);
+
 // Get an instance of the controller prefixed by HelloWorld
 $controller = JControllerLegacy::getInstance('NoKWebDAV');
- 
+
 // Perform the Request task
 $input = JFactory::getApplication()->input;
 $controller->execute($input->getCmd('task'));
- 
+
 // Redirect if set by the controller
 $controller->redirect();
 ?>
