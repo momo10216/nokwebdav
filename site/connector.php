625edb85223435a06cb6c7770eef7d8075d9115f
Norbert Kuemin <momo_102@bluemail.ch>
2017-07-28 17:05:50 +0200

    First version of connector

---
+++ site/connector.php
@@ -0,0 +1,51 @@
+<?php
+function getLocation() {
+	global $_SERVER;
+	$location = str_replace($_SERVER["SCRIPT_INFO"],'',$_SERVER["PATH_INFO"]);
+	return $location;
+}
+$component = 'com_nokwebdav';
+
+define('_JEXEC', 1);
+define('JPATH_BASE', explode('/components/'.$component,__DIR__)[0]);
+define('JPATH_COMPONENT', JPATH_BASE . DIRECTORY_SEPARATOR . 'components'. DIRECTORY_SEPARATOR . $component);
+include_once (JPATH_BASE.'/includes/defines.php' );
+if (!defined('JPATH_ADMINISTRATOR')) {
+	define('JPATH_ADMINISTRATOR', JPATH_BASE . DIRECTORY_SEPARATOR . 'administrator');
+}
+define('JPATH_COMPONENT_ADMINISTRATOR', JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components'. DIRECTORY_SEPARATOR . $component);
+
+require_once (JPATH_BASE.'/includes/framework.php' );
+jimport( 'joomla.application.application' );
+jimport( 'joomla.filter.filteroutput' );
+JDEBUG ? JProfiler::getInstance('Application')->setStart($startTime, $startMem)->mark('afterLoad') : null;
+$app = JFactory::getApplication('site');
+// Logging
+jimport('joomla.log.log');
+JLog::addLogger(
+	array(
+		'text_file' => 'nokwebdav.log',
+		'text_file_path' => '/tmp/',
+		'text_entry_format' => '{DATETIME} {PRIORITY} {MESSAGE}'
+	),
+	JLog::ALL
+);
+
+/*
+// Init controller
+jimport('joomla.application.component.controller');
+$controller = JControllerLegacy::getInstance('NoKWebDAV');
+*/
+
+JLoader::register('WebDAVHelper', JPATH_COMPONENT_ADMINISTRATOR.'/helpers/webdav.php', true);
+$type = 'files';
+$access = array('read' => true, 'create' => true, 'change' => true, 'delete' => true);
+$fileLocation = '/';
+$contactData = array();
+$eventData = array();
+$webdavHelper = WebDAVHelper::getInstance($type, $access, $fileLocation, $contactData, $eventData);
+$webdavHelper->run();
+
+// Exit
+$app->close();
+?>
