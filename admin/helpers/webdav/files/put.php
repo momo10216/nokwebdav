0ceb0dab8a31a91880d66336484dfad3a2ab6516
Norbert Kuemin <momo_102@bluemail.ch>
2017-08-14 14:14:37 +0100

    Refactoring put for large files

--- admin/helpers/webdav/files/put.php
+++ admin/helpers/webdav/files/put.php
@@ -14,47 +14,47 @@ defined('_JEXEC') or die('Restricted access');
  
 class WebDAVHelperPluginCommand {
 	public static function execute($fileLocation, $rootLocation, $maxSize) {
-		$content = file_get_contents('php://input');
-		$status = self::_check($fileLocation, $rootLocation, $content, $maxSize);
+		$status = self::_check($fileLocation, $rootLocation, $maxSize);
 		$header = array();
 		$answer = '';
 		if (!$status) { $status = self::_save($fileLocation, $content); }
 		return array($status, $header, $answer);
 	}
 
-	private static function _check($fileLocation, $rootLocation, $content, $maxSize) {
+	private static function _check($fileLocation, $rootLocation, $maxSize) {
 		global $_SERVER;
 		if (is_dir($fileLocation)) { return WebDAVHelper::$HTTP_STATUS_ERROR_CONFLICT; }
 		if (WebDAVHelper::isLocked('files', $fileLocation)) {
 			WebDAVHelper::debugAddMessage('File locked: '.$fileLocation);
 			return WebDAVHelper::$HTTP_STATUS_ERROR_LOCKED;
 		}
-		if (!WebDAVHelperPlugin::hasEnoughSpace($rootLocation,strlen($content),$maxSize)) {
+		if (!WebDAVHelperPlugin::hasEnoughSpace($rootLocation,$_SERVER['CONTENT_LENGTH'],$maxSize)) {
 			WebDAVHelper::debugAddMessage('Not enough space');
 			return WebDAVHelper::$HTTP_STATUS_ERROR_INSUFFICIENT_STORAGE;
 		}
 		return '';
 	}
 
-	private static function _save($fileLocation, $content) {
+	private static function _save($fileLocation) {
 		$fileIsNew = !file_exists($fileLocation);
 		if (!$fileIsNew && !is_writable ($fileLocation)) {
 			WebDAVHelper::debugAddMessage('Cannot write to file: '.$fileLocation);
 			return WebDAVHelper::$HTTP_STATUS_ERROR_FORBIDDEN;
 		}
-		$fh = fopen($fileLocation, 'wb');
 		if (!$fileIsNew && !is_writable ($fileLocation)) {
 			WebDAVHelper::debugAddMessage('Cannot open file for writing: '.$fileLocation);
 			return WebDAVHelper::$HTTP_STATUS_ERROR_FORBIDDEN;
 		}
-		if (strlen($content) > 0) {
-			if (!fwrite($fh, $content)) {
-				fclose($fh);
-				WebDAVHelper::debugAddMessage('Cannot write content to file: '.$fileLocation);
-				return WebDAVHelper::$HTTP_STATUS_ERROR_FORBIDDEN;
-			}
+		$fhWrite = fopen($fileLocation, 'wb');
+		$fhRead = fopen('php://input', 'rb');
+		if (!stream_copy_to_stream($fhRead, $fhWrite)) {
+			fclose($fhRead);
+			fclose($fhWrite);
+			WebDAVHelper::debugAddMessage('Cannot write content to file: '.$fileLocation);
+			return WebDAVHelper::$HTTP_STATUS_ERROR_FORBIDDEN;
 		}
-		fclose($fh);
+		fclose($fhRead);
+		fclose($fhWrite);
 		return $fileIsNew ? WebDAVHelper::$HTTP_STATUS_CREATED : WebDAVHelper::$HTTP_STATUS_NO_CONTENT;
 	}
 }
