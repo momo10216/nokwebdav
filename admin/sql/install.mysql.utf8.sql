190fa4975a9d543969a1bebdbdc12c39742a9031
Norbert Kuemin <norbert@kuem.in>
2017-10-27 15:48:52 +0100

    Add new infos to ical

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -65,3 +65,15 @@ CREATE TABLE `#__nokWebDAV_datas` (
 	`modifieddate` datetime NOT NULL default '0000-00-00 00:00:00',
 	PRIMARY KEY  (`id`)
 ) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `#__nokWebDAV_data_keys` (
+	`id` integer NOT NULL auto_increment,
+	`data_id` integer NOT NULL,
+	`key` varchar(50) NOT NULL,
+	`value` varchar(255) NOT NULL,
+	`createdby` varchar(50) NULL default NULL,
+	`createddate` datetime NULL default NULL,
+	`modifiedby` varchar(50) NOT NULL default '',
+	`modifieddate` datetime NOT NULL default '0000-00-00 00:00:00',
+	PRIMARY KEY  (`id`)
+) DEFAULT CHARSET=utf8;
